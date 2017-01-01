57946f693d7ccb7f1b70f5824b6a10c0740fa1f3
SSSvensk <sss2341@hotmail.com>
2017-01-01 23:46:36 +0200

    Point calculation added

--- js/mousefunctions.js
+++ js/mousefunctions.js
@@ -10,11 +10,11 @@
 		raycaster.setFromCamera( mouse, camera );
 
 		//If object is selected and object is inside yard, let's run the following code
-		if ( SELECTED && (SELECTED.position.x < 14.5 && SELECTED.position.x > -14.5 && SELECTED.position.z < 14.5 && SELECTED.position.z > -14.5 )) {
+		if ( SELECTED ) { //&& (SELECTED.position.x < 14.5 && SELECTED.position.x > -14.5 && SELECTED.position.z < 14.5 && SELECTED.position.z > -14.5 )) {
 
 			//Getting 0-1-value from function that inspects if objects are on top of each other
-            var ot = checkObjectsNotTopOfEachOther();
-			if ( raycaster.ray.intersectPlane( plane, intersection ) && ot == 0) {
+            //var ot = checkObjectsNotTopOfEachOther();
+			if ( raycaster.ray.intersectPlane( plane, intersection ) ) {
 
 				//Not sure excatly what here happens, but apparently position of object is changing here
 				SELECTED.position.copy( intersection.sub( offset ) );
@@ -37,21 +37,24 @@
 			if ( raycaster.ray.intersectPlane( plane, intersection )) {
 
 				ADDED.position.copy( intersection.sub( offset ) );
-
+				render();
+               
 				//Rendering while mouse is dragging object and position is inside boundaries
-				if ((ADDED.position.x < 14.5 && ADDED.position.x > -14.5) && (ADDED.position.z < 14.5 && ADDED.position.z > -14.5)) {
+				//if ((ADDED.position.x < 14.5 && ADDED.position.x > -14.5) && (ADDED.position.z < 14.5 && ADDED.position.z > -14.5)) {
 					render();
-				}
+				//}
 		    } 
 		    return;
 		}
 
 		//Else if object is selected but outside yard limits
 		//Then it's returned back to its previous location before drag&drop and no more selected and alerted to user
+		/*
 		else if ( SELECTED && (SELECTED.position.x > 14.5 || SELECTED.position.x < -14.5 || SELECTED.position.z > 14.5 || SELECTED.position.z < -14.5 )) {
 			cancelDragAndDrop();
 			alert("Object over bounds!");
 		}
+		*/
 
 		//Storing every object, that are penetrated by ray, to intersects-array
 		var intersects = raycaster.intersectObjects( objects, true);
@@ -123,6 +126,14 @@
 		}
 
 		container.style.cursor = 'auto';
+		var points = 0;
+		for (var i = 0; i < objects.length; i++) {
+			for (var j = i+1; j < objects.length; j++) {
+				points += getDistance(objects[i].userData.OBID, objects[j].userData.OBID);
+			};
+	    };
+	    calculatePoints();
+	    //document.getElementById('points').innerHTML = points.toFixed(0);
 		render();
 
 	}
@@ -132,10 +143,18 @@
 		event.preventDefault();
 		controls.enabled = true;
 		//If new object has been just added and it's inside yard borders
-		if (ADDED && ((ADDED.position.x < 14.5 && ADDED.position.x > -14.5) && (ADDED.position.z < 14.5 && ADDED.position.z > -14.5))) {
-			//insertObject();
+		if (ADDED ) { //&& ((ADDED.position.x < 14.5 && ADDED.position.x > -14.5) && (ADDED.position.z < 14.5 && ADDED.position.z > -14.5))) {
+			calculatePoints( ADDED );
 			SELECTED = null;
 			ADDED = null;
+			var points = 0;
+			for (var i = 0; i < objects.length; i++) {
+			    for (var j = i+1; j < objects.length; j++) {
+				    points += getDistance(objects[i].userData.OBID, objects[j].userData.OBID);
+			    };
+		    };
+		    calculatePoints();
+	        //document.getElementById('points').innerHTML = points.toFixed(0);
 			render();
 			//return;
 		} else {
@@ -144,8 +163,8 @@
 		    raycaster.setFromCamera( mouse, camera );
 		    var intersects = raycaster.intersectObjects( objects, true );
 		    if (INTERSECTED && !dragged) {
-			    var xco = INTERSECTED.parent.position.z.toFixed(2); 
-			    var zco = INTERSECTED.parent.position.x.toFixed(2);
+			    var xco = INTERSECTED.parent.matrixWorld.getPosition().x + INTERSECTED.parent.userData.REX; 
+			    var zco = INTERSECTED.parent.matrixWorld.getPosition().z + INTERSECTED.parent.userData.REZ;
 			    var obidi = INTERSECTED.parent.userData.OBID;
 			    document.getElementById('oi').innerHTML = obidi;
 			    document.getElementById('success').title = INTERSECTED.parent.userData.TYPE;
